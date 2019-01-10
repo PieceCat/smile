6cd68e01994b5147ff4c34eb16df2dab6e4cf35d
chenzhengyu <2677244229@qq.com>
2019-01-10 18:00:47 +0800

    使用koa-bodyparser中间件

--- demo/koa2/index.js
+++ demo/koa2/index.js
@@ -1,18 +1,23 @@
 const Koa = require('koa')
 const app = new Koa()
+const bodyParser = require('koa-bodyparser')
 
-app.use(async (ctx)=>{
-    let url = ctx.url
-    let request = ctx.request
-    let req_query = request.query
-    let req_querystring = request.querystring
-    ctx.body = {
-        url,
-        req_query
-        req_querystring
+app.use(bodyParser())
+
+app.use(async(ctx)=>{
+    if(ctx.url==='/'&&ctx.method==='GET'){
+        let html = `
+        <h1>JSPang Koa2 request POST</h1>
+            <form method="POST" action="/">
+                <p>userName</p>
+                <input name="userName" /><br/>
+                <p>age</p>
+                <input name="age" /><br/>
+                <p>website</p>
+                <input name="webSite" /><br/>
+                <button type="submit">submit</button>
+            </form>
+        `
+        ctx.body = html
     }
 })
-
-app.listen(3000)
-
-console.log('[demo] start-quick is starting at port 3000')
